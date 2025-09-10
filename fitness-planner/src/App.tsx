e0791fefc956ca3a9f30895956c96ba5ac86a1c7
nikolavukovic210gimpp-ctrl <nikola.vukovic210.gimpp@gmail.com>
2025-09-10 19:35:09 +0200

    App route & Blank Pages

--- fitness-planner/src/App.tsx
+++ fitness-planner/src/App.tsx
@@ -1,15 +1,23 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Dashboard from './pages/Dashboard';
+import Planner from './pages/Planner';
+import ExerciseExplorer from './pages/ExerciseExplorer';
+import Journal from './pages/Journal';
+
+function App() {
   return (
     <>
-      <div>
-        Fitness Planner
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/planner' element={<Planner />} />
+          <Route path='/exercise-explorer' element={<ExerciseExplorer />} />
+          <Route path='/journal' element={<Journal />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ fitness-planner/src/pages/ExerciseExplorer.tsx
@@ -0,0 +1,5 @@
+const ExerciseExplorer = () => {
+  return <div>ExerciseExplorer</div>;
+};
+
+export default ExerciseExplorer;
